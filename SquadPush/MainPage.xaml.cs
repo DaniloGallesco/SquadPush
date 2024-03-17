55a78ba897a9253336a4e5104c4cc583a48e0c1f
Danilo Cicotti Gallesco <danilo0201@hotmail.com>
2024-03-17 09:17:51 -0300

    Add project files.

---
+++ SquadPush/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Plugin.Firebase.CloudMessaging;
+
+namespace SquadPush
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
+            var token = await CrossFirebaseCloudMessaging.Current.GetTokenAsync(); 
+            
+            LblToken.Text = token;
+
+
+            SemanticScreenReader.Announce(LblToken.Text);
+#if DEBUG
+            Console.WriteLine("TOKEN: "+token);
+#endif
+        }
+    }
+
+}
---
+++ SquadPush/MauiProgram.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+#if IOS
+using Plugin.Firebase.Bundled.Platforms.iOS;
+#else
+using Plugin.Firebase.Bundled.Platforms.Android;
+#endif
+
+namespace SquadPush
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .RegisterFirebaseServices()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                return false;
+            }));
+#else
+                events.AddAndroid(android => android.OnCreate((activity, _) =>
+                    CrossFirebase.Initialize(activity, CreateCrossFirebaseSettings())));
+#endif
+            });
+
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+            return builder;
+        }
+
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(isAuthEnabled: true);
+        }
+
+    }
+}
